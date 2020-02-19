2797519d07d65de08e258ee4dc71af76823e6141
Brandon Douglas Johnson <brandojd93@gmail.com>
2020-02-19 16:48:20 -0600

    Good enough.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,29 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+   @task =Task.new(task_strong_params)
+   @task.save
+   redirect_to tasks_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+
+  private
+
+  def task_strong_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+end
+
